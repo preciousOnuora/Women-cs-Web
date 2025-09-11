2e4628802cb4dc6a1bc8198086b50587be92832a
preiousOnuora <onuoraprecious@gmail.com>
2025-09-11 14:19:19 +0100

    Fix deployment issues with simplified app

---
+++ src/MinimalApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MinimalApp() {
+  return (
+    <div>
+      <h1>Minimal Test</h1>
+      <p>This is the most basic React app possible.</p>
+    </div>
+  );
+}
+
+export default MinimalApp;
