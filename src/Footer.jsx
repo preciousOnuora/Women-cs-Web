3b7167473f9f625f3ac2174ea5fe46edf32fca38
preiousOnuora <onuoraprecious@gmail.com>
2025-09-13 19:04:06 +0100

    Fix dashboard events loading and navbar size

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -31,6 +31,7 @@ const Footer = () => {
             <li><a href="/">Home</a></li>
             <li><a href="/events">Events</a></li>
             <li><a href="/gallery">Gallery</a></li>
+            <li><a href="/contact">Contact</a></li>
             <li><a href="/sponsors">Sponsors</a></li>
             <li><a href="/feedback">Feedback</a></li>
           </ul>
